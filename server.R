2b5db737ebbbf7c92cce642e3cca6695f89a7fc9
HITFuture <brett.r.taylor@icloud.com>
2016-01-16 17:59:51 -0700

    Just began building the Portal

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 # This is the server logic for a Shiny web application.
 # You can find out more about building applications with Shiny here:
@@ -7,44 +7,149 @@
 #
 
 library(shiny)
+library(scales)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(rMaps)
+library(rCharts)
+#library(leaflet)
+library(RColorBrewer)
+library(maps)
+library(mapproj)
+library(DT)
+library(pryr)
+ library(lineprof)
+library(gdata)
 
-shinyServer(function(input, output) {
+messageData <- data.frame(from=c("brettt@slhs.org","wilfordtr@slhs.org"),message=c("Hey!","What?"))
+
+
+# if (packageVersion('shiny') > '0.7') {
+#         library(shiny)
+#         runGitHub("shiny-examples", "rstudio", subdir = "012-datatables")
+# }
+
+#require(devtools)
+#install_github('ramnathv/rCharts@dev')
+#install_github('ramnathv/rMaps')
+#devtools::install_github('rstudio/shinyapps')
+#devtools::install_github('rstudio/DT')
+#install.packages("leaflet")
+
+function(input, output,session) {
+        print(session)
+   #Reactive Functions
+        autoInvalidate <- reactiveTimer(1000,session = session)
+observe({
+        autoInvalidate()
+})
+
+
+
+performanceValues <- reactiveValues(predict.time = 0)
+
+#This is a test
+predictedWords <- reactive({
+         performanceValues$predict.time <- system.time({
+         Sys.sleep(rnorm(1,.5,.3))  
+         })
+        textEntered()
+})
+ textEntered <- eventReactive(input$submitText, {
+         input$textEntry
+ })
+         
+  output$messageMenu <- renderMenu({
+    # Code to generate each of the messageItems here, in a list. This assumes
+    # that messageData is a data frame with two columns, 'from' and 'message'.
+    msgs <- apply(messageData, 1, function(row) {
+      messageItem(from = row[["from"]], message = row[["message"]])
+    })
+    dropdownMenu(type = "messages", .list = msgs)
+    
+  }
+  )
+  
+ tasks <- list( taskItem(value = 90, color = "green",
+           "Documentation"
+  ),
+  taskItem(value = 17, color = "aqua",
+           "Project X"
+  ),
+  taskItem(value = 75, color = "yellow",
+           "Server deployment"
+  ),
+  taskItem(value = 80, color = "red",
+           "Overall project"
+  )
+  )
  
-#   output$dataEntry <- textInput("textSource", "Input:")
-
-  output$textAccuracyPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = 20 + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+ output$taskMenu <- renderMenu({
+   dropdownMenu(type = "tasks", .list = tasks,badgeStatus = "success")
+   
+ }
+ )
+    
+ output$urlText <- renderText({
+         paste(sep = "",
+               "protocol: ", session$clientData$url_protocol, "\n",
+               "hostname: ", session$clientData$url_hostname, "\n",
+               "pathname: ", session$clientData$url_pathname, "\n",
+               "port: ",     session$clientData$url_port,     "\n",
+               "search: ",   session$clientData$url_search,   "\n"
+         )
+ })
+ output$memInfo <- renderInfoBox({
+         autoInvalidate()
+         infoBox("R Memory", humanReadable(mem_used(),standard="SI"), subtitle="Delta",icon = icon("arrow-up"),color = "red" ,width = 3)
+ })
+ output$predictInfo <- renderInfoBox({
+          infoBox("Prediction Info", paste(round(performanceValues$predict.time["elapsed"],2), "sec"), subtitle="Average",icon = icon("arrow-up"),color = "red" ,width = 3)
+ })
+ output$textPredictionOut <- renderText({
+         predictedWords()
+        
+ })
+  plotUtilizationByTime <-  renderPlot({
+    laterst<-store.transactions%>%filter(StartedOnDate > as.Date("2015-07-16"))
+    ggplot(laterst, aes(x=StartedOnDate,fill=StartedOnDayOfWeek)) + 
+      geom_bar() + 
+      scale_x_date(breaks=date_breaks(width="7 day"))+
+      theme_bw()+
+      theme( axis.text.x = element_text( angle = 45))    
   })
-
-})
-=======
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
-library(shiny)
-
-shinyServer(function(input, output) {
-
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  
+  output$plotAccessHistory <- renderPlot({
+   
+  ggplot(access.history.by.date, aes(x=launchStartDate,y=count,fill=isMicrosoftOffice)) + 
+    geom_bar(stat = "identity") + 
+    scale_x_date(breaks=date_breaks(width="7 day"))+
+    theme_bw()+
+    theme( axis.text.x = element_text( angle = 45))  
+    
   })
+  
+   
+  output$plotTopUser <- renderPlot({
+#     topUser <-  
+#       user.access.office$AD_userId[1]
+#     top1.access.history <- 
+#     ggplot(top1.access.history , aes(x=launchStartDate,fill=isMicrosoftOffice)) + 
+#       stat_bin(binwidth=1, position="identity") + 
+#     scale_x_date(breaks=date_breaks(width="7 day"))+
+#       theme_bw()+
+      theme( axis.text.x = element_text( angle = 45))
+  })
+  
+  output$storeData <- DT::renderDataTable({
+    data <- store.transactions[1:1000,]%>%select(-contains("guid",ignore.case=TRUE) )
+    if(nrow(data) == 0)return(datatable(data))
+    
+    datatable(data , extensions = c("ColReorder",'ColVis','Responsive' ), options = list(dom = 'RC<"clear">lfrtip',pageLength=20, autoWidth = TRUE,
+                                                                                         colVis = list(exclude = c(0, 1), activate = 'mouseover')
+    ) )
+    
+  })
+}
 
-})
--- ui.R
+++ ui.R
@@ -1,4 +1,8 @@
-<<<<<<< HEAD
+
+
+
+
+
 
 # This is the user-interface definition of a Shiny web application.
 # You can find out more about building applications with Shiny here:
@@ -7,52 +11,89 @@
 #
 
 library(shiny)
+library(shinydashboard)
+library(DT)
+require(rCharts)
+library(pryr)
+library(lineprof)
+#options(RCHART_LIB = 'polycharts')
 
-shinyUI(fluidPage(
 
-  # Application title
-  titlePanel("Text Prediction"),
+sidebar <-         dashboardSidebar(
+        headerPanel("Text Prediction")  ,
+        sidebarMenu(
+                menuItem("Predict", tabName = "predict",icon = icon("cloud")),
+                menuItem("Configuration", tabName = "config",icon = icon("setting")),
+                menuItem("Help", tabName = "help", icon = icon("question-circle"))
+        ) 
+)
+ 
+body <-  dashboardBody(
+        tags$head(
+                tags$link(rel = "stylesheet", type = "text/css", 
+                          href = "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.cs")
+        ),
+        tabItems(
+                tabItem(tabName = "predict",
+                        fluidRow(infoBoxOutput("memInfo",width = 3),
+                                 infoBoxOutput("predictInfo",width = 3),
+                                infoBox("Total Predictions", 15.3, subtitle="Deviation",icon = icon("refresh"),color="yellow",width = 3),
+                                infoBox("Number of Sessions", 1, subtitle="Concurrent",icon = icon("refresh" ),color = "green",width = 3)
+                               
+                        ),
+                        
+                        
+                        fluidRow(
+                                box(title = "Text Submission",
+                                    
+                                    textInput("textEntry",label="Enter Text",width = "50%"),
+                                    actionButton("submitText","Predict!",icon = icon("star")), 
+                                    color="blue",width = 12)
+                        
+                        ),
+                        fluidRow(
+                                box(title = "Text Prediction",textOutput("textPredictionOut"),width = 12) 
+                        )
+                ),
+                tabItem(tabName = "config",
+                        
+                        fluidRow(
+                                box(title = "Config Sessions",
+                                    textOutput("config1Out"), width = 12)
+                        ),
+                        fluidRow(
+                                box(title = "Config Targets",plotOutput("plotStoreTransactions"), width = 12)
+                                
+                        )
+                ),
+                
+                tabItem(tabName = "help",
+                        h1("Text Prediction Information")
+                        
+                )))
 
-  # Sidebar with entry of text
-  sidebarLayout(
-    sidebarPanel(
-      textInput("dataEntry","Enter Text" )
-    ),
-
-    # Show a plot of accuracy of the prediction
-    mainPanel(
-      plotOutput("textAccuracyPlot")
-    )
-  )
-))
-=======
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
-library(shiny)
-
-shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-))
+dashboardPage(
+        
+        dashboardHeader(title = "Data Science Capstone",
+                        dropdownMenuOutput("messageMenu"),
+                        dropdownMenuOutput("taskMenu"),
+                        
+                        dropdownMenu(
+                                type = "notifications",
+                                notificationItem(
+                                        text = "5 new users today",
+                                        icon("users")
+                                ),
+                                notificationItem(
+                                        text = "12 items delivered",
+                                        icon("truck"),
+                                        status = "success"
+                                ),
+                                notificationItem(
+                                        text = "Server load at 86%",
+                                        icon = icon("exclamation-triangle"),
+                                        status = "warning"
+                                )
+                        )),
+        sidebar,
+        body)
